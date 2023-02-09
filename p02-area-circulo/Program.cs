56268f986f74adc38e3729258b18814ce70fe697
Migue085 <miguelinbadillo@gmail.com>
2023-02-09 11:47:35 -0600

    Actividad 1

---
+++ p02-area-circulo/Program.cs
@@ -0,0 +1,13 @@
+﻿// Calcula el area de un circulo
+
+double area, radio;
+Console.WriteLine("Calclando el area de un circulo");
+Console.Write("Dame el radio >");
+radio = double.Parse(Console.ReadLine());
+area = Math.PI * Math.Pow(radio,2);
+Console.WriteLine($"El circulo de radio {radio} tiene una area de {area}\n");
+Console.WriteLine("El tipo de dato de las variables es:");
+
+Console.WriteLine(radio.GetType());
+Console.WriteLine(area.GetType());
+
