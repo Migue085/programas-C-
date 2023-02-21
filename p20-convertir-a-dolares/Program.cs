db67268f78cf1006babfcbbe50055483cd6e0e13
Migue085 <miguelinbadillo@gmail.com>
2023-02-21 10:50:49 -0600

    tarea 1

--- p20-convertir-a-dolares/Program.cs
+++ p20-convertir-a-dolares/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Dada una cantidad en pesos y la cotización del dólar, se desea obtener el equivalente en dólares.
 
-double pesos, dolar, equivalente;
+double pesos, dolar, cot;
 
 Console.Clear();
 Console.WriteLine("Ingrese la cantidad en pesos:");
 pesos = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Ingrese la cotización del dólar:");
-dolar = double.Parse(Console.ReadLine());
+dolar = 18.3970;
 
-equivalente = pesos / dolar;
+cot = pesos / dolar;
 
-Console.WriteLine("Equivalente en dólares: " + equivalente);
+Console.WriteLine("Equivalente en dólares: " + cot);
