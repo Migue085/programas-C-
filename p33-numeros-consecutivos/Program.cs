3d0960bdd3ae77c088db4e46a38ceb6b99002101
Migue085 <miguelinbadillo@gmail.com>
2023-03-05 18:04:44 -0600

    New

--- p33-numeros-consecutivos/Program.cs
+++ p33-numeros-consecutivos/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Dados tres números enteros, verificar si son consecutivos (9,10,11) y mandar mensaje confirmándolo, si no lo son
 //(1,4,6) mandar mensaje de error.
-Console.WriteLine("Hola mundo");
+
+int num1,num2,num3;
+
+Console.Clear();
+Console.WriteLine("Introduzca el primer numero");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduzca el segundo numero");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduzca el tercer numero");
+num3 = int.Parse(Console.ReadLine());
+
+if(num1 + 1 == num2 && num2 + 1 == num3){
+    Console.WriteLine("Los Numeros son consecutivos!");
+}else{
+    Console.WriteLine("Lo siento, los numeros no son consecutivos");
+}
