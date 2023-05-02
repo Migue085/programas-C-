93d889754f6f132d6f87e992f0d52f6596a0fbc0
Migue085 <miguelinbadillo@gmail.com>
2023-05-02 11:47:44 -0600

    act 15

--- p85-cuenta-bancaria-v1/Banco.cs
+++ p85-cuenta-bancaria-v1/Banco.cs
@@ -4,7 +4,7 @@ public string Direccion {get; private set;}
 public List<Cliente> Clientes {get; private set;}
 
 public Banco() => Clientes = new List<Cliente>();
-public Banco(string nombre, string domicilio) : this() => (Nombre,Direccion) = (nombre,domicilio);
+public Banco(string nombre, string direccion) : this() => (Nombre,Direccion) = (nombre,direccion);
 public void AgregarCliente(Cliente cliente) => Clientes.Add(cliente);
 public override string ToString() => $"{Nombre}, {Direccion}";
 }
